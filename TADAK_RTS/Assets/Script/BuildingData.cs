1d3a78658948dc40eb1c76e45c4f8fcf83710c63
Kim JinWook <dpscbvls8358@naver.com>
2025-12-18 19:18:01 +0900

    관련 변수 변경
    기능 요구서에 맞춰서 클래스 변수 변경

--- TADAK_RTS/Assets/Script/BuildingData.cs
+++ TADAK_RTS/Assets/Script/BuildingData.cs
@@ -8,20 +8,17 @@ public enum Race { Human, Elf, Beastman, Undead }
 public abstract class BuildingData {
     public string ID { get; protected set; }
     public Race Race { get; protected set; }
-    public int Gold { get; protected set; }
+    public int Wood { get; protected set; }
+    public int Rock { get; protected set; }
     public float MaxHealth { get; protected set; }
     public float BuildTime {get; protected set; }
-
-    public float Armor {get; protected set; }
-    public float SupplyProvided {get; protected set; }
-    public List<string> ProducibleUnits { get; protected set; }
 
 
 }
 
 
 public class BaseBuildingData : BuildingData {
-    public BaseBuildingData(string id, Race race, int gold, float hp, float armor, float supplyprovided, List<string> producibleUnits) {
-        ID = id; Race = race; Gold = gold; MaxHealth = hp; Armor = armor; SupplyProvided = supplyprovided; ProducibleUnits = producibleUnits;
+    public BaseBuildingData(string id, Race race, int wood, int rock, float hp, float buildTime) {
+        ID = id; Race = race; Wood = wood; Rock = rock; MaxHealth = hp; BuildTime = buildTime;
     }
 }
--- TADAK_RTS/Assets/Script/BuildingDatabase.cs
+++ TADAK_RTS/Assets/Script/BuildingDatabase.cs
@@ -4,10 +4,10 @@ public static class BuildingDatabase {
     private static readonly Dictionary<string, BuildingData> _db = new Dictionary<string, BuildingData>();
 
     static BuildingDatabase() {
-        // 아이디, 종족, int 소모 골드, float 체력, float 방어력, int 인구수 공급량, List<string> 생산 가능 유닛
-        Add("Human_Barracks", Race.Human, 150, 500f, 0f, 0, null);
-        Add("Elf_Tree", Race.Elf, 200, 800f, 0f, 0, null);
-        Add("Orc_Den", Race.Beastman, 120, 600f, 0f, 0, null);
+        // 아이디, 종족, int 소모 목재, int 소모 석재, float 체력, 건설시간
+        Add("Human_Barracks", Race.Human, 150, 150, 500f, 15f);
+        Add("Elf_Tree", Race.Elf, 200, 200, 800f, 0f);
+        Add("Orc_Den", Race.Beastman, 120, 120, 600f, 0f);
 
 
 
@@ -15,8 +15,8 @@ public static class BuildingDatabase {
 
     }
 
-    private static void Add(string id, Race race, int gold, float hp, float armor, float supplyprovided, List<string> producibleUnits) {
-        _db.Add(id, new BaseBuildingData(id, race, gold, hp, armor, supplyprovided, producibleUnits));
+    private static void Add(string id, Race race, int wood, int rock, float hp, float buildTime) {
+        _db.Add(id, new BaseBuildingData(id, race, wood, rock, hp, buildTime));
     }
 
     public static BuildingData Get(string id) {
