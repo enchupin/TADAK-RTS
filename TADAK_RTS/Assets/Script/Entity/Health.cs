9ccbfbcdc364095369895ad99dfbbd845a5f0821
Kim JinWook <dpscbvls8358@naver.com>
2026-01-01 16:48:01 +0900

    건물 체력 구현
    실제 인게임에서 건물의 체력이 동작하는 방식 구현
    
    Health클래스를 공용으로 사용할 수 있게하고 BuildingHealth클래스가 Health를 상속받는 방식으로 구현
    
    굳이 이렇게 구현해야되나 싶긴했는데 일단 혹시 모르니까 상속으로 구현함

---
+++ TADAK_RTS/Assets/Script/Entity/Health.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public abstract class Health : MonoBehaviour
+{
+    public float CurrentHp { get; protected set; }
+    public float MaxHp { get; protected set; }
+
+    // 체력이 변할 때 UI 등에 알릴 이벤트
+    public System.Action<float, float> OnHpChanged;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public virtual void Initialize(float maxHp) {
+        MaxHp = maxHp;
+        CurrentHp = maxHp;
+    }
+    public virtual void TakeDamage(float amount) {
+        CurrentHp -= amount;
+        CurrentHp = Mathf.Clamp(CurrentHp, 0, MaxHp);
+
+        // UI에 알림
+        OnHpChanged?.Invoke(CurrentHp, MaxHp);
+
+        if (CurrentHp <= 0) Die();
+    }
+
+    protected abstract void Die();
+
+}
