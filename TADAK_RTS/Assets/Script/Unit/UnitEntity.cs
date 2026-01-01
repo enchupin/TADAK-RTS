4d901f04cb1e5aa278de8a516f4a56935988d1fa
Kim JinWook <dpscbvls8358@naver.com>
2026-01-01 18:57:15 +0900

    UnitEntity 구현
    UnitEntity 클래스가 MonoBehaviour를 상속하도록 구현

---
+++ TADAK_RTS/Assets/Script/Unit/UnitEntity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class UnitEntity : MonoBehaviour
+{
+    public BaseUnit Data { get; private set; }
+
+
+
+    public void Init(BaseUnit unitData) {
+        this.Data = unitData;
+
+        // OnDead 구독
+        if (this.Data is IDead deadable) {
+            deadable.OnDead += (unit) => OnDestroyVisual();
+        }
+    }
+
+    public void TakeDamage(float damage) {
+        Data.TakeDamage(damage);
+    }
+
+    private void OnDestroyVisual() {
+        Destroy(gameObject);
+    }
+
+}
