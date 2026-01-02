4bdf74a3ebf4add1167e2056cb942b3f454da541
Kim JinWook <dpscbvls8358@naver.com>
2026-01-02 19:10:31 +0900

    유닛 선택 구현

--- TADAK_RTS/Assets/Script/GameManager/ISeclectable.cs
+++ TADAK_RTS/Assets/Script/GameManager/ISeclectable.cs
@@ -3,4 +3,6 @@ using System.Collections.Generic;
 
 public interface ISelectable {
 
+    // 개체 UI 표시
+    void SingleSelectEntityInfo();
 }
---
+++ TADAK_RTS/Assets/Script/GameManager/SelectedUnit.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SelectedUnits : MonoBehaviour {
+    public static SelectedUnits Instance { get; private set; }
+
+    // 현재 선택된 유닛 리스트
+    private List<UnitController> _selectedList = new List<UnitController>();
+    public int Count => _selectedList.Count;
+
+    private void Awake() {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    // 선택된 유닛 초기화
+    public void Clear() {
+
+        foreach (var unit in _selectedList) {
+            // 유닛 선택 시점에 유닛에 추가된 시각적 효과가 있다면 해제하는 기능 구현
+        }
+        _selectedList.Clear();
+    }
+
+    // 유닛 추가
+    public void Add(UnitController unit) {
+        if (!_selectedList.Contains(unit)) {
+            _selectedList.Add(unit);
+        }
+    }
+
+    // 선택된 유닛들에게 이동 명령 하달
+    public void ExecuteMove(Vector3 destination) {
+        foreach (var unit in _selectedList) {
+            IMovement movement = unit.GetComponent<IMovement>();
+            if (movement != null) {
+
+                // movement.Move();
+
+            }
+        }
+    }
+
+
+    // 선택 상태에 따른 UI 업데이트
+    public void UpdateSelectionUI() {
+        if (_selectedList.Count == 0) return;
+    }
+
+
+}
